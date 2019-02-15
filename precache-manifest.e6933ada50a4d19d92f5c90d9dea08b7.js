ddbbf882bea16450657fbd972d8246bc3bb156c0
JKBC1800 <31428649+JKBC1800@users.noreply.github.com>
2019-02-15 13:38:33 +1100

    Updates

---
+++ precache-manifest.e6933ada50a4d19d92f5c90d9dea08b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/contactmanager/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "7722ffc62d6b1e316f29",
+    "url": "/contactmanager/static/js/main.7722ffc6.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/contactmanager/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "7722ffc62d6b1e316f29",
+    "url": "/contactmanager/static/css/main.da9d3606.chunk.css"
+  },
+  {
+    "revision": "5dacdee85e24cdb6e9b4a6766fe22138",
+    "url": "/contactmanager/index.html"
+  }
+];
