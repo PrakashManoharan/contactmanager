3f3400cf70125e2e123f488be9870f75d83f7505
prakash Manoharan <prakash.manoharan@outlook.com>
2022-07-25 13:08:29 +1000

    modified the copy table

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React in a hour with Sanjaya
           </a>
         </header>
       </div>
